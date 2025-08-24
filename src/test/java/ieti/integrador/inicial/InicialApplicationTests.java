a6260a42b9955dc13f6e52805eaa97c5661b3eb7
ManuelB16 <felipebarrera104@gmail.com>
2025-08-24 17:33:05 -0500

    Repo inicial

---
+++ src/test/java/ieti/integrador/inicial/InicialApplicationTests.java
@@ -0,0 +1,13 @@
+package ieti.integrador.inicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
